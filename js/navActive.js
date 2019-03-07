132f550b03ecca28e876b6bc8ce0337c8e432a29
NicolaiHjacobsen <43333217+NicolaiHjacobsen@users.noreply.github.com>
2019-03-07 15:01:02 +0100

    responsivenes begyndt

---
+++ js/navActive.js
@@ -0,0 +1,12 @@
+(function()
+{   
+    var menu = document.querySelector('ul'),
+        menulink = document.querySelector('i');
+
+    menulink.addEventListener('click', function(e)
+    {
+        menu.classList.toggle('active')
+        e.preventDefault();
+    })
+
+})();
